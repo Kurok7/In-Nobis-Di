717393b6ab4c39ca21fd78ce08e7b533b7676eed
Juan Malone <malonejuank@gmail.com>
2022-02-17 06:10:20 -0800

    Modified Readme.md and added IND Arc 1 website

---
+++ IND Arc 1 Website/script.js
@@ -0,0 +1,28 @@
+//cleanup code to make it more presentable
+
+function Eliza(obj) {
+    obj.innerHTML = "Zeus!"
+}
+function Azarius(obj) {
+    obj.innerHTML = "Eliza Azarius"
+}
+
+function Aeni(obj) {
+    obj.innerHTML = "Odin!"
+}
+function Silverlake(obj) {
+    obj.innerHTML = "Aeni Silverlake"
+}
+
+function Jenine(obj) {
+    obj.innerHTML = "Amaterasu!"
+}
+function Zantaella(obj) {
+    obj.innerHTML = "Jenine Zantaella"
+}
+function Akiko(obj) {
+    obj.innerHTML = "Gaia!"
+}
+function Hikari(obj) {
+    obj.innerHTML = "Akiko Hikari"
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # In-Nobis-Di
+
+This is a starter website for In Nobis Di, a graphic novel collaboration between me and my best friend. I intend to use this as a starting point for my web development career. The information in this will be updated as I complete the story and my friend finishes the drawings. 
